3aef6e35e360a1ca6b03aa988b3342a28d1b0188
DragonQ <dragonq29@gmail.com>
2020-02-07 21:09:07 +0900

    마이바티스 설정 적용

--- src/main/java/board/configuration/DatabaseConfiguration.java
+++ src/main/java/board/configuration/DatabaseConfiguration.java
@@ -28,6 +28,12 @@ public class DatabaseConfiguration {
   }
 
   @Bean
+  @ConfigurationProperties(prefix = "mybatis.configuration")
+  public org.apache.ibatis.session.Configuration mybatisConfig() {
+    return new org.apache.ibatis.session.Configuration();
+  }
+
+  @Bean
   public DataSource dataSource() throws Exception {
     DataSource dataSource = new HikariDataSource(hikariConfig());
     System.out.println(dataSource.toString());
